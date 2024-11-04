0ef83c3a3c3bcaf7961f774f8c9753a1d04d12a0
motion2015 <motion74@naver.com>
2024-11-05 00:07:07 +0900

    initial commit

---
+++ src/layout/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <div class="botinfo">
+      <div class="botbtngroup">
+        <button class="btn-text">찾아오시는 길</button>
+        <button class="btn-text">개인정보취급방침</button>
+        <button class="btn-text">이용 약관</button>
+        <button class="btn-gray">저자 신청</button>
+        <button class="btn-gray">역자 신청</button>
+        <button class="btn-gray">교재 샘플/강의자료 신청</button>
+      </div>
+    </div>
+    <div class="footerinfo">
+      <div class="footer-logo">
+        <img src="/images/footer-logo.png" alt="" />
+      </div>
+      <div class="footer-sub">
+        <address>
+          서울시 마포구 잔다로로 109 이지퍼블리싱 <span>Tel 02-1234-1234</span>
+        </address>
+        <p>Email email@email.com</p>
+        <p>통신판매업 신고 : 2020-서울강남-001234</p>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ src/layout/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="wrap">
+    <h1 class="blind">웹사이트 제목</h1>
+    <Header />
+    <article id="container">
+      <router-view />
+    </article>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
